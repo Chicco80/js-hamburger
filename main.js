322b76b8956e02fb040857a6a384674c02ebb3f2
Chicco80 <chicco80lisa@gmail.com>
2022-03-27 15:48:11 +0200

    da correggere

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-let apriti = document.querySelector(".header-right");
-console.log(apriti);
+let apriti = document.querySelector(".fas.fa-bars");
+// console.log(apriti);
+
+function mine(){
+    let menu = document.querySelector(".hamburger-menu");
+    menu.classList.add("active");
+    
+}
+
+apriti.addEventListener("click", mine);
+
+
+
+let chiudi = document.querySelector(".close");
+
+function mine2(){
+    let menu = document.querySelector(".hamburger-menu");
+    menu.classList.remove("active");
+    // console.log(menu);
+}
+
+chiudi.addEventListener("clicK", mine2);
